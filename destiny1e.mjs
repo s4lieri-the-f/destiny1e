a87f853eb1451e881492756509ebb67c3685fcb0
s4lieri <aprasidze.gera@gmail.com>
2024-12-17 14:06:51 +0300

    removed test item template

--- destiny1e.mjs
+++ destiny1e.mjs
@@ -1,7 +1,7 @@
 import D1ItemSheet from "./module/sheets/item-sheet.mjs"
 
 Hooks.once("init", function(){
-    console.log("destiny1e | Initiation of Destiny system 1e");
+    console.log("Destiny Game System 1e | Initiation of Destiny system 1e");
 
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("d1e", D1ItemSheet, { makeDefault: true} )
